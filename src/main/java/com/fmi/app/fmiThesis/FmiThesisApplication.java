975090b46060180ed7952c83fb318617a3054041
alexandraivan97 <alexandraivan97@gmail.com>
2018-05-07 22:14:10 +0300

    First commit

---
+++ src/main/java/com/fmi/app/fmiThesis/FmiThesisApplication.java
@@ -0,0 +1,12 @@
+package com.fmi.app.fmiThesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmiThesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmiThesisApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fmi/app/fmiThesis/FmiThesisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fmi.app.fmiThesis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FmiThesisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
